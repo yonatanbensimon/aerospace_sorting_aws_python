95336155bf6875a3b323d315a45ff83ad19e290d
yonatanbensimon <106496877+yonatanbensimon@users.noreply.github.com>
2024-02-22 16:28:50 +0000

    Modified App.js

--- sort-data/src/App.js
+++ sort-data/src/App.js
@@ -4,7 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <p>Welcome to my web app</p>
+      <h1>Sort Data Service</h1>
+      <p>Welcome to the sort data service, where you can upload different types of data, sort them in different ways, and finally redownload your data. Keep in mind that this service is still under development</p>
+      <form>
+        <label><input type="radio" name="datatype" text="Phone Number"></input></label>
+        <label><input type="radio" name="datatype" text="Adresses"></input></label>
+        <label><input type="radio" name="datatype" text="Turbines and IBR's"></input></label>
+      </form>
     </div>
   );
 }
