5097990f2631c17e90b0977102675d258d9cf065
yonatanbensimon <106496877+yonatanbensimon@users.noreply.github.com>
2024-02-22 22:26:17 +0000

    Radiobuttons work

--- sort-data/src/App.js
+++ sort-data/src/App.js
@@ -2,23 +2,30 @@ import './App.css';
 import Phone from './Phone';
 import Address from './Address';
 import Turbine from './Turbine';
-import {useState} from React;
+
 import React from 'react';
+import { useState } from 'react';
 
 function App() {
+  const [selectedForm, setSelectedForm] = useState(null);
+
+  const handleChange = (event) => {
+    setSelectedForm(event.target.value);
+  }
+
   return (
     <div className="App">
       <h1>Sort Data Service</h1>
       <p>Welcome to the sort data service, where you can upload different types of data, sort them in different ways, and finally redownload your data. Keep in mind that this service is still under development</p>
       <h2>Type of Data</h2>
       <form>
-        <label><input type="radio" name="datatype" text="Phone Number"></input>Phone Number</label>
-        <label><input type="radio" name="datatype" text="Adresses"></input>Adresses</label>
-        <label><input type="radio" name="datatype" text="Turbines and IBR's"></input>Turbines and IBR's</label>
+        <label><input type="radio" name="datatype" text="Phone Number" value="phone" checked={selectedForm === "phone"} onChange={handleChange}></input>Phone Number</label>
+        <label><input type="radio" name="datatype" text="Adresses" value="address" checked={selectedForm === "address"} onChange={handleChange}></input>Addresses</label>
+        <label><input type="radio" name="datatype" text="Turbines and IBR's" value="turbine" checked={selectedForm === "turbine"} onChange={handleChange}></input>Turbines and IBR's</label>
       </form>
-      <Phone />
-      <Address />
-      <Turbine />
+      {selectedForm === "phone" && <Phone />}
+      {selectedForm === "address" && <Address />}
+      {selectedForm === "turbine" && <Turbine />}
     </div>
     
   );
