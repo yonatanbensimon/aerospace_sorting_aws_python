96d8705a27e9dbbe7a45235df89bdddfe75444b6
yonatanbensimon <106496877+yonatanbensimon@users.noreply.github.com>
2024-02-22 22:53:46 +0000

    Added Amplify

---
+++ sort-data/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,28 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "sortdata7c047254": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "S3Trigger50ee68cd": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "S3": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
